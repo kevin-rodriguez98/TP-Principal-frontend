f057c4665b340190524038aea6f401f71be98af6
Aldana Micaela <aldana.filiberto17@gmail.com>
2025-10-02 22:01:53 -0300

    -

--- sistema-de-gestion/vite.config.ts
+++ sistema-de-gestion/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true,   // 👈 esto habilita el acceso desde tu red
+    port: 5173    // opcional, podés cambiar el puerto
+  }
 })
