3363b03f3c5cf200b1b72ebda2b9c94c512d80a6
Juli Nuñez <njulianacamila14@gmail.com>
2025-10-30 00:33:41 -0300

    add estilos a boton iniciar sesión

--- sistema-de-gestion/src/components/Header.tsx
+++ sistema-de-gestion/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Nav.css';
 import Notificaciones from './Notificaciones';
+import { Login as LoginIcon } from "@mui/icons-material";
 import LoginFace from '../login/FaceLogin'; // componente login facial
 
 const Header = () => {
@@ -17,8 +18,12 @@ const Header = () => {
     <>
       <nav className="navbar">
         <img className='title' src='./logo-blanco.png' onClick={() => navigate("/")} />
+        <div className='div-notify'>
+        <button className="btn-login" onClick={() => handleLoginSuccess()}>
+          <LoginIcon className="icon-login" />
+        </button>
         <Notificaciones />
-        <button onClick={() => handleLoginSuccess()}>Iniciar sesión</button>
+        </div>
       </nav>
     </>
   );
